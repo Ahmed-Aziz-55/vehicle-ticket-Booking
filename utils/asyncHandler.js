50a7f17fa96d0f169ed15bbd7802ae83ec659e14
Ahmed-Aziz-55 <AhmedAziz55223@gmail.com>
2026-02-26 22:14:49 +0500

    fix 1

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -1,9 +1,6 @@
 // utils/asyncHandler.js
 export const asyncHandler = (fn) => {
     return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch((error) => {
-            console.error('🔥 Async Handler Error:', error);
-            next(error);
-        });
+        Promise.resolve(fn(req, res, next)).catch(next);
     };
 };
